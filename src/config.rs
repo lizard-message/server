418b55fde56d3c019abdda2441fbae86b926cb64
neetdai <974498346@qq.com>
2020-09-13 18:57:39 +0800

    添加配置

--- src/config.rs
+++ src/config.rs
@@ -1,12 +1,11 @@
 use serde_derive::Deserialize;
-use toml::de::{Error as TomlError};
 use std::fs::File;
 use std::io::{BufReader, Error as IoError, Read};
 use thiserror::Error;
+use toml::de::Error as TomlError;
 
 #[derive(Debug, Error)]
 pub enum Error {
-
     #[error("io error `{0}`")]
     Io(#[from] IoError),
 
@@ -24,12 +23,10 @@ impl Config {
         let file_handle = File::open(config_path)?;
         let mut reader = BufReader::new(file_handle);
         let mut buff = Vec::new();
-        
+
         reader.read_to_end(&mut buff)?;
 
-        Ok(
-            toml::from_slice(&buff)?
-        )
+        Ok(toml::from_slice(&buff)?)
     }
 
     pub fn get_client_config(&self) -> &Client {
@@ -40,10 +37,12 @@ impl Config {
 #[derive(Deserialize)]
 pub struct Client {
     host: String,
-
     port: u16,
-
-    support_ssl: bool,
+    support_tls: bool,
+    support_push: bool,
+    support_pull: bool,
+    support_compress: bool,
+    max_message_length: u32,
 }
 
 impl Client {
@@ -55,7 +54,23 @@ impl Client {
         &self.port
     }
 
-    pub fn get_support_ssl(&self) -> &bool {
-        &self.support_ssl
+    pub fn get_support_tls(&self) -> &bool {
+        &self.support_tls
+    }
+
+    pub fn get_support_push(&self) -> &bool {
+        &self.support_push
+    }
+
+    pub fn get_support_pull(&self) -> &bool {
+        &self.support_pull
+    }
+
+    pub fn get_support_compress(&self) -> &bool {
+        &self.support_compress
+    }
+
+    pub fn get_max_message_length(&self) -> &u32 {
+        &self.max_message_length
     }
 }
