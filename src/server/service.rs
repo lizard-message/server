7ab9c201df5b375dda19c0eb272316b40ba6e760
phs <>
2020-09-15 18:01:32 +0800

    未完成

--- src/server/service.rs
+++ src/server/service.rs
@@ -2,26 +2,42 @@ use super::read_stream::{ReadStream, StreamType};
 use super::write_stream::WriteStream;
 use crate::global_static::CONFIG;
 use protocol::encode::ServerConfig;
+use protocol::decode::{Decode, Error as DecodeError, Message};
+use protocol::state::Support;
 use std::io::Error as IoError;
 use thiserror::Error;
 use tokio::io::{split, AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpStream;
+use futures::stream::StreamExt;
+use log::{error, info};
 
 #[derive(Debug, Error)]
 pub(super) enum Error {
     #[error("io error `{0}`")]
     Io(#[from] IoError),
+
+    #[error("hand shake error")]
+    HandShake,
+}
+
+#[derive(Debug)]
+enum Mode {
+    OnlyPush,
+    OnlyPull,
+    PushAndPull,
 }
 
 #[derive(Debug)]
 pub(super) struct Service {
     read_stream: ReadStream,
     write_stream: WriteStream,
+    mode: Mode,
 }
 
 impl Service {
     pub(super) async fn new(stream: TcpStream) -> Result<Self, Error> {
         let client_config = CONFIG.get_client_config();
+        
         let mut server_config = ServerConfig::default();
 
         if *client_config.get_support_push() {
@@ -40,16 +56,63 @@ impl Service {
             server_config.support_compress();
         }
 
-        stream.write(server_config.encode().as_slice()).await?;
+        server_config.max_message_length(*client_config.get_max_message_length());
 
-        let (mut read_stream, mut write_stream) = split(stream);
-        let mut read_stream = ReadStream::new(read_stream);
-        let mut write_stream = WriteStream::new(wirte_stream);
+        stream.write(&server_config.encode().freeze()).await?;
 
-        Ok(Self {
-            read_stream,
-            write_stream,
-        })
+        let mut decode = Decode::new(*client_config.get_max_message_length() as usize);
+
+        loop {
+            match stream.read_buf(decode.get_mut_buffer()).await {
+                Ok(0) => {
+                    return Err(Error::HandShake);
+                },
+                Ok(_) => {
+                    if let Some(result) = decode.next().await {
+                        match result {
+                            Ok(message) => {
+                                if let Message::Info(version, mask, max_message_size) = message {
+
+                                    let mode = {
+                                        if mask & Support::Push && mask & Support::Pull {
+                                            Mode::PushAndPull
+                                        } else {
+                                            if mask & Support::Push {
+                                                Mode::OnlyPush
+                                            } else if mask & Support::Pull {
+                                                Mode::OnlyPull
+                                            } else {
+                                                return Err(Error::HandShake);
+                                            }
+                                        }
+                                    };
+
+                                    let (mut read_stream, mut write_stream) = split(stream);
+                                    let mut read_stream = ReadStream::new(read_stream);
+                                    let mut write_stream = WriteStream::new(wirte_stream);
+
+                                    return Ok(Self {
+                                        read_stream,
+                                        write_stream,
+                                        mode,
+                                    });
+                                } else {
+                                    error!("message error: not client info in hand shake");
+                                }
+                            }
+                            Err(e) => {
+                                return Err(e);
+                            }
+                        }
+                    } else {
+                        continue;
+                    }
+                }
+                Err(e) => {
+                    return Err(e.into());
+                }
+            }
+        }
     }
 
     pub(super) async fn run(mut self) {}
